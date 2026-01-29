439b167be2d52bf84b3bf11799703c9d14b479d0
Asyed-82 <104303485+Asyed-82@users.noreply.github.com>
2026-01-29 11:54:17 -0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// Firebase Configuration - ADD THIS TO BOTH FILES
+const firebaseConfig = {
+    apiKey: "AIzaSyYOUR_API_KEY_HERE",
+    authDomain: "your-project-id.firebaseapp.com",
+    projectId: "your-project-id",
+    storageBucket: "your-project-id.appspot.com",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "1:YOUR_APP_ID:web:YOUR_APP_ID"
+};
